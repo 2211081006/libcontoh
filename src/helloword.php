240c47e99ea36d597972b11005495865f18a1512
2211081006 <cindysteffani0@gmail.com>
2024-05-29 09:20:05 +0700

    commit pertama

---
+++ src/helloword.php
@@ -0,0 +1,9 @@
+<?
+
+namespace cindy\libhallo;
+
+class helloworld{
+    public function  sayhello($name){
+        return "Hello $name welcome to php composer to create your own library";
+    }
+}
